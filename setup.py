d9964a462c6813ab9d2ba433532ff7801f495ca2
Vladislav.Verba <Vladislav.Verba@x5.ru>
2020-09-04 17:12:49 +0300

    Add setup draft

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+# TODO: add more info
+
+setup(
+    name="mind-mapper",
+    version="0.1",
+    packages=find_packages(),
+    # install_requires=[],
+    # entry_points="""
+    #     [console_scripts]
+    # """,
+)

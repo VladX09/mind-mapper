9d76410b28fc4e8726aa0c51c5b70e2d0413e556
Vladislav.Verba <Vladislav.Verba@x5.ru>
2020-09-04 22:09:14 +0300

    Add package_data and entrypoint

--- setup.py
+++ setup.py
@@ -5,8 +5,10 @@ setup(
     name="mind-mapper",
     version="0.1",
     packages=find_packages(),
+    package_data={"mind_mapper": ["themes/*.yml"]},
     # install_requires=[],
-    # entry_points="""
-    #     [console_scripts]
-    # """,
+    entry_points="""
+        [console_scripts]
+        mind-mapper=cli:render
+    """,
 )

2596e6751ce41e6de3ee5a9917f112ed35193e11
Vladislav.Verba <Vladislav.Verba@x5.ru>
2020-09-05 17:42:30 +0300

    Add file loading logging

--- mind_mapper/utils.py
+++ mind_mapper/utils.py
@@ -2,10 +2,14 @@ import os
 from importlib import resources as importlib_resources
 
 import yaml
+from loguru import logger
 
 
 def load(path):
     if os.path.exists(path):
+        path = os.path.abspath(path)
+
+        logger.debug("Loading from '{}'", path)
         with open(path) as fp:
             return yaml.full_load(fp)
 
@@ -14,6 +18,7 @@ def load(path):
 
     try:
         with importlib_resources.open_text(package, name) as fp:
+            logger.debug("Loading from '{}' in package '{}'", name, package)
             return yaml.full_load(fp)
 
     except FileNotFoundError:

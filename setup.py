5f49cbb9f8bce96907868d2bd6adb7319e8e3019
Vladislav.Verba <Vladislav.Verba@x5.ru>
2020-09-05 17:50:21 +0300

    Add dependencies

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
-from setuptools import setup, find_packages
-# TODO: add more info
+import os
+
+from setuptools import find_packages, setup
+
+
+def parse_requirements(requirements_file: str):
+    requirements_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), requirements_file)
+
+    with open(requirements_path, "r") as fp:
+        return list(fp.readlines())
+
 
 setup(
     name="mind-mapper",
     version="0.1",
     packages=find_packages(),
     package_data={"mind_mapper": ["themes/*.yml"]},
-    # install_requires=[],
+    install_requires=parse_requirements("requirements.in"),
     entry_points="""
         [console_scripts]
         mind-mapper=mind_mapper.cli:render
